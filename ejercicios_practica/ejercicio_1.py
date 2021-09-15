86af30b6a9152412e63c87c7b22084a5180111d3
Alejandro L. Cardona <acardona@unc.edu.ar>
2021-09-14 22:04:01 -0300

    Update ejercicio_1.py

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -11,6 +11,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def line_plot(x, y, title, fx):
+    fig = plt.figure()
+    fig.suptitle(title, fontsize=16)
+    ax = fig.add_subplot()
+
+    ax.plot(x, y, c='darkgreen', label=fx)
+    ax.legend()
+    ax.grid()
+    plt.show(block=False)
+    print("Fin line plot")
+
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
@@ -35,7 +46,11 @@ if __name__ == '__main__':
 
     # Colocar la leyenda y el label con el nombre de la función
     # Darle color a la línea a su elección
+    leg = 'Funcióm Cuadrática'
+    label = 'f(x)=x**2'
 
     # Crear acá su gráfico
+
+    grafico = line_plot(x, y, leg, label)
 
     print("terminamos")
