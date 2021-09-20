d771ccd8c376223f0172d54df1f36e4dfa14ac3e
Alejandro L. Cardona <acardona@unc.edu.ar>
2021-09-20 13:07:29 -0300

    Update ejercicio_2.py

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -11,6 +11,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def two_plt(x, xlabel, y1, y2, ylabel, title, label1, label2):
+    # general
+    fig = plt.figure()
+    fx = fig.add_subplot()
+    fx.set_title(title, fontsize=16)
+    fx.set_facecolor('whitesmoke')
+    fx.set_ylabel(ylabel)
+    fx.set_xlabel(xlabel)
+
+    #plot 1
+    fx.plot(x, y1, color='b', marker='^', label=label1)
+    #plot 2
+    fx.plot(x, y2, color='c', marker='+', label=label2)
+    fx.legend()
+    plt.show()
+
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
@@ -18,7 +34,7 @@ if __name__ == '__main__':
 
     # NOTA: aproveche los ejemplos "multi_line_plot" de clase
 
-    # Alumno: Se desea graficar varias funciones en un mismmo gráfico (axe)
+    # Alumno: Se desea graficar varias funciones en un mismmo gráfico (fige)
 
     # Las funciones que se desean implementar y graficar son:
     # y1 = x**2
@@ -36,7 +52,7 @@ if __name__ == '__main__':
         y2.append(i**3)
 
     # Realizar un gráfico que representen las dos funciones
-    # Para ello se debe llamar dos veces a "plot" con el mismo "ax"
+    # Para ello se debe llamar dos veces a "plot" con el mismo "fig"
 
     # Se debe colocar en la leyenda la función que representa
     # cada función
@@ -45,5 +61,7 @@ if __name__ == '__main__':
     # a su elección
 
     # Crear acá su gráfico
+
+    graf = two_plt(x, 'x', y1, y2, 'f(x)', 'Dos Funciones', 'y**2', 'y**3')
 
     print("terminamos")
